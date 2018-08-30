9bd23217c3fa10171e8198598e953fd2b838d402
MikaQ56 <mickaelquintin@me.com>
2018-08-30 12:00:50 +0200

    Activity 1 from OCLesson implemented

---
+++ Miawouf/Controller/CatSuccessViewController.swift
@@ -0,0 +1,18 @@
+//
+//  CatSuccessViewController.swift
+//  Miawouf
+//
+//  Created by Mickael on 30/08/2018.
+//  Copyright © 2018 Mickael. All rights reserved.
+//
+
+import UIKit
+
+class CatSuccessViewController: UIViewController {
+
+    
+    @IBAction func dismiss() {
+        dismiss(animated: true, completion: nil)
+    }
+    
+}
--- Miawouf/Controller/CatWelcomeViewController.swift
+++ Miawouf/Controller/CatWelcomeViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class CatWelcomeViewController: UIViewController {
 
-   
+    @IBAction func unwindToCatWelcome(segue: UIStoryboardSegue) {
+        
+    }
 
 }
