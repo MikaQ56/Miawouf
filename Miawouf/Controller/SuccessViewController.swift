72e39a347ece21ce452c950f531e90179568ac15
MikaQ56 <mickaelquintin@me.com>
2018-08-30 10:29:25 +0200

    Dismiss on successView added & unwind segue added for newRegistration button

--- Miawouf/Controller/SuccessViewController.swift
+++ Miawouf/Controller/SuccessViewController.swift
@@ -21,7 +21,10 @@ class SuccessViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-
+    @IBAction func dismiss() {
+        dismiss(animated: true, completion: nil)
+    }
+    
     /*
     // MARK: - Navigation
 
